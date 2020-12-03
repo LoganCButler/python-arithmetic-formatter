b1dea16fb797d107d38019cfcd66a2a40f1f5a87
Logan Butler <logan.butler@agilethought.com>
2020-12-03 14:50:00 -0500

    add class methods for shape dimentions

--- polygon-area-calculator/shape_calculator.py
+++ polygon-area-calculator/shape_calculator.py
@@ -5,7 +5,28 @@ class Rectangle:
         self.height = height
 
     def __str__(self):
-         return f'width:{self.width} height:{self.height}'
+         return f'Rectangle(width={self.width}, height={self.height})'
+
+    def set_width(self, width):
+        self.width = width
+    
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.height * self.width
+    
+    def get_perimeter(self):
+        return (self.height * 2) + (self.width * 2)
+    
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_picture(self):
+        pass #TODO: not implemented
+
+    def get_amount_inside(self):
+        pass #TODO: not implemented
 
 
 
@@ -13,9 +34,19 @@ class Square(Rectangle):
     # default constructor 
     def __init__(self, sideLength): 
         super().__init__(sideLength, sideLength)
+    
+    def set_side(self, side):
+        self.set_width(side)
+        self.set_height(side)
 
+    def __str__(self):
+        return f'Square(side={self.width})'
 
 
 
 rect = Rectangle(5, 3)
+rect.set_width(9)
 print(rect)
+print(rect.get_diagonal())
+
+print(Square(2))
