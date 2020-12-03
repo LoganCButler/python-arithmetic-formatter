2228125a596681f6a183d0678d355051e91b8f2b
Logan Butler <logan.butler@agilethought.com>
2020-12-03 14:25:28 -0500

    add ctr for rectangle and square

--- polygon-area-calculator/shape_calculator.py
+++ polygon-area-calculator/shape_calculator.py
@@ -1,6 +1,21 @@
 class Rectangle:
+    # default constructor 
+    def __init__(self, width, height): 
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+         return f'width:{self.width} height:{self.height}'
+
+
+
+class Square(Rectangle):
+    # default constructor 
+    def __init__(self, sideLength): 
+        super().__init__(sideLength, sideLength)
 
 
 
 
-class Square:
+rect = Rectangle(5, 3)
+print(rect)
